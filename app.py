8ca7718edaf7bda51ec9ddce0c8acea9aab0d193
evr1k <rohitshmahato@gmail.com>
2020-02-22 01:02:49 +0530

    Json Test

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
 
+@app.route('/hithere')
+def hi_there_everyone():
+    return "I just hit /hithere!"
+
+@app.route('/bye')
+def bye():
+    retJson = {
+        'field1': 'abc',
+        'field2': 'def'
+    }
+
+    return jsonify(retJson)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
